b03b48cceb233574bd59eeb2531a6de6712a3605
Meng Zhu <meng@g.clemson.edu>
2021-04-03 13:18:44 -0700

    expand devicons mode to filename and sizemtime

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
 import ranger.api
 from ranger.core.linemode import LinemodeBase
+from ranger.ext.human_readable import human_readable, human_readable_time
+from datetime import datetime
 from .devicons import *
 
 @ranger.api.register_linemode
@@ -10,3 +12,25 @@ class DevIconsLinemode(LinemodeBase):
 
   def filetitle(self, file, metadata):
     return devicon(file) + ' ' + file.relative_path
+
+@ranger.api.register_linemode
+class DevIconsLinemodeFile(LinemodeBase):
+  name = "filename"
+
+  def filetitle(self, file, metadata):
+    return devicon(file) + ' ' + file.relative_path
+
+@ranger.api.register_linemode
+class DevIconsLinemode(LinemodeBase):
+  name = "sizemtime"
+
+  uses_metadata = False
+
+  def filetitle(self, file, metadata):
+    return devicon(file) + ' ' + file.relative_path
+
+  def infostring(self, fobj, metadata):
+    if fobj.stat is None:
+      return '?'
+    return "%s %s" % (human_readable(fobj.size),
+                      datetime.fromtimestamp(fobj.stat.st_mtime).strftime("%Y-%m-%d %H:%M"))
